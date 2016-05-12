f04542f16bf10db51061ac177e2ecd939cf78f18
yxb_1990 <657048548@qq.com>
2016-05-12 17:12:15 +0800

    添加能回滚的单元测试

--- src/test/java/com/zz/springbootweb/test/UserServiceTest.java
+++ src/test/java/com/zz/springbootweb/test/UserServiceTest.java
@@ -9,12 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import java.util.Date;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
 @WebIntegrationTest
+@Transactional
 public class UserServiceTest {
 
     @Autowired
@@ -23,10 +26,12 @@ public class UserServiceTest {
     @Test
     public void test_createUser(){
         User user = new User();
-        user.setUsername("yxb");
+        user.setUsername("yxb2");
         user.setPlainPassword("111111");
         user.setCreationTime(new Date());
 
         userService.createUser(user);
+
+        Assert.isTrue(user.getId() > 0);
     }
 }
