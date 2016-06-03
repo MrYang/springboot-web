a4f41c76a24f5904a4303a07562b79ed9a1d5bf2
yxb_1990 <657048548@qq.com>
2016-06-03 09:53:29 +0800

    删除目录src

--- src/main/java/com/zz/springbootweb/repository/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.zz.springbootweb.repository;
-
-import com.zz.springbootweb.entity.User;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDao extends BaseDao<User, Long> {
-}
